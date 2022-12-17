4ae33a4b6f968081c12698b478601bd4d63e7126
devarend <devarend@gmail.com>
2022-12-17 04:13:20 +0100

    created custom CTAModal

--- ui/components/modal/CTAModal.tsx
+++ ui/components/modal/CTAModal.tsx
@@ -1,17 +1,19 @@
 import {FC} from "react";
+import Button from "../button/Button";
 
-const CTAModal: FC<CTAModalProps> = ({id, title, description, onClick, buttonText}) => {
+const CTAModal: FC<CTAModalProps> = ({id, title, description, onClick, buttonText, isLoading = false, isVisible, close}) => {
+    const openModalClassName = isVisible ? 'modal-open' : null
     return (
         <>
-            <input type="checkbox" id={id} className="modal-toggle"/>
-            <label htmlFor={id} className="modal modal-bottom sm:modal-middle">
+            <input onChange={close} type="checkbox" id={id} className="invisible"/>
+            <label htmlFor={id} className={`modal modal-bottom sm:modal-middle ${openModalClassName}`}>
                 <label className="modal-box relative" htmlFor="">
                     <label htmlFor={id}
                            className="btn btn-sm btn-circle absolute right-2 top-2 btn-primary text-black">✕</label>
                     <h3 className="text-lg font-bold">{title}</h3>
                     <p className="py-2">{description}</p>
                     <div className="modal-action">
-                        <button onClick={onClick} className="btn btn-primary text-black">{buttonText}</button>
+                        <Button isLoading={isLoading} onClick={onClick} className="btn-primary text-black">{buttonText}</Button>
                     </div>
                 </label>
             </label>
@@ -24,6 +26,9 @@ interface CTAModalProps {
     title: string
     description: string
     buttonText: string
+    isLoading?: boolean
+    isVisible: boolean
+    close(): void
 
     onClick(): void
 }
